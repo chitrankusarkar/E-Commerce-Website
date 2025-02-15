a53c0be06556d776adf32f8af46b49d311a408fd
Chitranku <chitrankusarkar1@gmail.com>
2025-02-15 22:09:26 +0530

    Fixed App.jsx and stire.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import AuthLayout from './components/auth/layout'
 
 function App() {
   return (
-   <div className="flex flex-col overflow-hidden bg-white">
-    <Routes>
-      <Route path='/auth' element={<AuthLayout />}></Route>
-    </Routes>
-   </div>
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/auth' element={<AuthLayout />} />
+      </Routes>
+    </div>
   )
 }
 
